59ac37bfa18693047282f5e5c70a6fae38de4690
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-07-06 22:50:43 +0600

    Add main application code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 #![allow(unused)]
 
+use actix_web::{App, HttpServer};
+use actix_web::middleware::Logger;
+use env_logger::Env;
+
+use crate::endpoints::{delete_flight_plan_by_id, file_flight_plan, get_all_flight_plan, get_flight_plan_by_id, update_flight_plan};
+
 mod schema;
 mod database;
 mod endpoints;
 
-fn main() {
-    println!("Hello, Actix Web!");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    HttpServer::new(move || {
+        App::new()
+            .service(get_flight_plan_by_id)
+            .service(get_all_flight_plan)
+            .service(delete_flight_plan_by_id)
+            .service(file_flight_plan)
+            .service(update_flight_plan)
+            .wrap(Logger::default())
+    })
+        .bind(("0.0.0.0", 3000))?
+        .workers(2)
+        .run()
+        .await
 }
